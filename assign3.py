c42187642de1851ac419a5a45c985e6884263da5
Swati77708 <65920520+Swati77708@users.noreply.github.com>
2020-09-23 15:21:31 +0530

    Add files via upload

---
+++ assign3.py
@@ -0,0 +1,54 @@
+	Assignment – 3
+	(20/09/2020)
+1)	Write a program to give values to two variables and check if it is a square or rectangle.
+
+ANS:	a=("square")
+	b=("rectangle")
+	if (a is "square") and (b is "rectangle"):
+    		print("True")
+	else:
+   		print("false")
+	True
+
+2)	Write a program to initiate two variables and store an integer value in them. Using proper loop print whether the first number is greater, or the second number is greater or both are equal.
+
+ANS:	a=56
+	b=63
+	if a>b:
+	    print("greater than")
+	elif(a<b):
+	    print("smaller than")
+	elif(a==b):
+	    print("equal to")
+	smaller than
+
+3)	Write a program to define age of three people and determine oldest and youngest among them.
+
+ANS:	p1=36
+	p2=23
+	p3=47
+	if p1>p2>p3 :
+   	 	print("p1 is older than p2 and p3")
+	elif p3>p1>p2 :
+		print("p3 is older than p1 and p2")
+	elif(p2>p3>p1) :
+    		print("p2 is older than p3 and p1")
+	p3 is older than p1 and p2
+
+4)	Write a program to assign value to a variable and then check if the number is even or odd.
+
+ANS:	x=45
+	if (x%2==0) :
+    		print("even")
+	else:
+   		print("odd")
+	odd
+
+5)	Mary wants to eat chocolate but she can only do that if it doesn’t rain. Otherwise she has to eat ice cream. Write a program to assign a statement “It is raining” to a variable and then decide if Mary should ice cream or chocolate?
+
+ANS:	x=("it is raining")
+	if x is ("it is raining"):
+    		print("mary should eat icecream")
+	else:
+    		print("mary should eat chocolate")
+	mary should eat icecream
